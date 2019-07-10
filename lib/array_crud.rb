7170983de41eac08e7faa9a3494296984c67ca37
ryanthornton628 <ryanthornton628@gmail.com>
2019-07-10 23:34:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -21,11 +21,12 @@ end
 
 def remove_element_from_start_of_array(array)
   array.shift
+  
 end
 
 def retrieve_element_from_index(array, index_number)
-  array
-  
+  array[index_number]
+
 end
 
 def retrieve_first_element_from_array(array)
